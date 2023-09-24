1029955f8d20eee5c35036a50cde264d4ea2cf0d
antar <333antar333@gmail.com>
2023-09-24 14:59:33 +0200

    Correction of comments

--- students/st_app/views.py
+++ students/st_app/views.py
@@ -20,14 +20,14 @@ def generate_student(request):
 
 def generate_students(request):
     count = int(request.GET.get('count', 10))
-    if count > 0:
+    if count <= 0:
+        return render(request, 'error.html', {'message': "Query параметр должен быть положительным числом"})
+    else:
         count = min(count, 100)
         for i in range(count):
             Student.objects.create(
                 first_name=fake.first_name(), last_name=fake.last_name(), birth_date=fake.date(pattern='%d-%m-%Y'))
         return render(request, 'generate_students.html', {'count': count})
-    else:
-        return render(request, 'error.html', {'message': "Query параметр должен быть положительным числом"})
 
 
 def students(request):

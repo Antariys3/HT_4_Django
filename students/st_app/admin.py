23ecbd4471d9be24a53490c229f2f00250c49364
antar <333antar333@gmail.com>
2023-10-07 13:01:13 +0200

    Домашнее задание 6. Создание класса Group. И выполнение второй части д\з.

--- students/st_app/admin.py
+++ students/st_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from st_app.models import Student
+from st_app.models import Student, Teacher, Group
 
 admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Group)
